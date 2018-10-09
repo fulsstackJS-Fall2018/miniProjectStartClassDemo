2aa1207cead37df4c8e26b13bbcb239036b32a23
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2018-10-09 11:11:53 +0200

    What we did in the class + a bit more

---
+++ settings.js
@@ -0,0 +1,9 @@
+const DEV_DB_URI = "mongodb://mp-user:mp1234@ds223253.mlab.com:23253/mp";
+const TEST_DB_URI = "mongodb://mp-test-user:mp1234@ds223253.mlab.com:23253/mp-test";
+const MOCHA_TEST_TIMEOUT = 5000;
+
+module.exports = {
+  DEV_DB_URI,
+  TEST_DB_URI,
+  MOCHA_TEST_TIMEOUT
+}
